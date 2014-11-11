f0f3de009891b406803a94d8b8e1035623dbb6e8
Prince <princebalabis@gmail.com>
2014-11-11 07:51:04 +0100

    about activity done

--- MyApplication/app/src/main/java/com/princetronics/peepy/AboutActivity.java
+++ MyApplication/app/src/main/java/com/princetronics/peepy/AboutActivity.java
@@ -1,17 +1,31 @@
 package com.princetronics.peepy;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 /**
  * Created by Prince on 11/11/2014.
  */
 public class AboutActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // Update fonts
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/font200.ttf");
+
+        TextView tvAbout = (TextView) findViewById(R.id.tvAbout);
+        tvAbout.setTypeface(typeFace);
+
+        TextView tvVersionNumber = (TextView) findViewById(R.id.tvVersionNumber);
+        tvVersionNumber.setTypeface(typeFace);
+
+        // Display version number
+        String versionName = BuildConfig.VERSION_NAME;
+        tvVersionNumber.setText(tvVersionNumber.getText() + " " + versionName);
     }
 }
