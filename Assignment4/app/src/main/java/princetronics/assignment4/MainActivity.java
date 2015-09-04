373545ccb1f5a65748ecf04127e0a5eba91d2b69
Prince <princebalabis@gmail.com>
2015-09-04 06:12:10 +0200

    basic looping autoplay done

--- Assignment4/app/src/main/java/princetronics/assignment4/MainActivity.java
+++ Assignment4/app/src/main/java/princetronics/assignment4/MainActivity.java
@@ -1,16 +1,31 @@
 package princetronics.assignment4;
 
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayers[] = new MediaPlayer[3];
+    private String songNames[] = {"Swedens National Anthem","Denmarks National Anthem","Finlands National Anthem"};
+    private int playlistPosition = 0;
+
+    private TextView tv_songName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        viewsInit();
+
+        mediaPlayerInit();
     }
 
     @Override
@@ -34,4 +49,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    void viewsInit() {
+        tv_songName = (TextView) findViewById(R.id.tv_songname);
+    }
+
+    void mediaPlayerInit() {
+        // Load songs
+        mediaPlayers[0] = MediaPlayer.create(this, R.raw.swedens_national_anthem);
+        mediaPlayers[1] = MediaPlayer.create(this, R.raw.denmarks_national_anthem);
+        mediaPlayers[2] = MediaPlayer.create(this, R.raw.finlands_national_anthem);
+
+        tv_songName.setText(songNames[playlistPosition]); // draw song title in app
+
+        for(int i = 0; i< 3; i++){
+            mediaPlayers[i].setLooping(true); // Enable music looping for each song
+        }
+
+        mediaPlayers[0].start(); // Play audio
+    }
+
 }
