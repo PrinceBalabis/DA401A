abd5b2cb055864268cccbf4342d3932dd1123004
Prince <princebalabis@gmail.com>
2015-09-04 07:21:35 +0200

    checkpoint

--- Assignment4/app/src/main/java/princetronics/assignment4/MainActivity.java
+++ Assignment4/app/src/main/java/princetronics/assignment4/MainActivity.java
@@ -21,9 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = new MyMediaPlayer(this);
-
-        viewsInit();
+        mediaPlayer = new MyMediaPlayer(this); // Start media player
+        viewsInit(); // initialize views
     }
 
     @Override
@@ -55,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
         tvSongTitle.setText(mediaPlayer.getCurrentSongTitle()); // draw song title in app
         tvStatus.setText("Playing"); // Update media player status
 
-        Button btnPlayPause = (Button) findViewById(R.id.btn_play_pause);
+        final Button btnPlayPause = (Button) findViewById(R.id.btn_play_pause);
+        btnPlayPause.setText("Pause");
         View.OnClickListener oclBtnPlayPause = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,9 +65,11 @@ public class MainActivity extends AppCompatActivity {
                 if (isPlaying){
                     Log.d("Button", "Song is unpaused");
                     tvStatus.setText("Playing");
+                    btnPlayPause.setText("Pause");
                 }else {
                     Log.d("Button", "Song is paused");
                     tvStatus.setText("Paused");
+                    btnPlayPause.setText("Play");
                 }
             }
         };
@@ -88,7 +90,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("Button", "Next button pressed!");
-
+                mediaPlayer.playNextSong();
+                tvSongTitle.setText(mediaPlayer.getCurrentSongTitle()); // draw song title in app
             }
         };
         btnNext.setOnClickListener(oclBtnNext);
--- Assignment4/app/src/main/java/princetronics/assignment4/MyMediaPlayer.java
+++ Assignment4/app/src/main/java/princetronics/assignment4/MyMediaPlayer.java
@@ -2,13 +2,20 @@ package princetronics.assignment4;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Created by Prince on 04-Sep-15.
  */
 public class MyMediaPlayer {
 
-    private MediaPlayer mediaPlayers[] = new MediaPlayer[3];
+    final private String TAG = "MyMediaPlayer";
+
+    private int amountOfSongs = 3; // Amount of songs in app
+    private MediaPlayer mp = new MediaPlayer();
+    private int playlistID[] = new int[amountOfSongs];
     private String songTitles[] = {"Swedens National Anthem", "Denmarks National Anthem", "Finlands National Anthem"};
     private int playlistPosition = 0;
     private Context context;
@@ -17,19 +24,28 @@ public class MyMediaPlayer {
     public MyMediaPlayer(Context context) {
         this.context = context;
 
-        // Load song
-        mediaPlayers[0] = MediaPlayer.create(context, R.raw.swedens_national_anthem);
-        mediaPlayers[1] = MediaPlayer.create(context, R.raw.denmarks_national_anthem);
-        mediaPlayers[2] = MediaPlayer.create(context, R.raw.finlands_national_anthem);
+        customPlaylistInit();
 
-        // Enable looping for all songs
-        for (int i = 0; i < 3; i++) {
-            mediaPlayers[i].setLooping(true); // Enable music looping for each song
-        }
+        // This listener starts the song whenever song has finished preparing
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            public void onPrepared(MediaPlayer mp) {
+                Log.d(TAG, "Prepare finished!");
+                mp.start();
+            }
+        });
 
-        mediaPlayers[playlistPosition].start(); // Start playing first song
+        mp.create(this.context, playlistID[playlistPosition]); // Load first songs
+        //mp.setLooping(true); // Enable music looping for each song
+
+
     }
 
+    void customPlaylistInit() {
+        // Load songs
+        playlistID[0] = R.raw.swedens_national_anthem;
+        playlistID[1] = R.raw.denmarks_national_anthem;
+        playlistID[2] = R.raw.finlands_national_anthem;
+    }
 
     // returns current song title
     String getCurrentSongTitle() {
@@ -37,13 +53,34 @@ public class MyMediaPlayer {
     }
 
     // returns false if paused, true if currently playing
-    boolean togglePlayPause(){
-        if(mediaPlayers[playlistPosition].isPlaying()){
-            mediaPlayers[playlistPosition].pause();
+    boolean togglePlayPause() {
+        if (mp.isPlaying()) {
+            mp.pause();
             return false;
         } else {
-            mediaPlayers[playlistPosition].start();
+            mp.start();
             return true;
         }
+    }
+
+    void playNextSong() {
+        mp.reset(); // Stop current song
+
+        // Go to next song or reset position to first song if position is at last song
+        if (playlistPosition < 2) {
+            playlistPosition++;
+        } else {
+            playlistPosition = 0;
+        }
+
+        mp.start(); // Start next song
+    }
+
+    void playPreviousSong() {
+
+    }
+
+    void stopSong() {
+
     }
 }
